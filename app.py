cf2699b82c7daeadf662e79007b3b6a3ed1343fd
PatriceAlan <peckpatricealan@gmail.com>
2023-09-26 20:47:02 +0200

    Fix some bugs

--- app.py
+++ app.py
@@ -35,6 +35,28 @@ def index():
 
     return render_template('index.html', **locals())
 
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form.get('title')
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit() 
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:id>')
+def update(id):
+    todo = Todo.query.filter_by(id=id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    todo = Todo.query.filter_by(id=id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 @app.route('/about')
 def about():
     return render_template('about.html')
